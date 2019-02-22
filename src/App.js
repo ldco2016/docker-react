dfc790a6d6538d5e9eb56f6d4a3376a01583e532
danale <danale@danales-MacBook-Pro.local>
2019-02-22 14:42:16 -0600

    changed app text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I had to mv Dockerfile.dev Dockerfile
-          </p>
+          <p>Teaching Junior Devs a Professional Workflow with Git</p>
           <a
             className="App-link"
             href="https://reactjs.org"
